30ada94da4ad94d6af690720d4eaf091376d3a9b
uwuflanny <gianlumiglia@gmail.com>
2022-09-10 01:18:13 +0200

    started map chunk rendering

--- misc/map.js
+++ misc/map.js
@@ -9,56 +9,111 @@ var colors = [
     '#FFFFFF'  // 301 - 500  HAZARDOUS
 ];
 
-// lat -90  to 90
-// lng -180 to 180
-var marker_clusters = {};
-var MERIDIANS = 24;
-var PARALLELS = 12;
-function get_marker_grid_position(lat, lng) {
-    let meridian = Math.floor((lng + 180) / 360 * MERIDIANS);
-    let parallel = Math.floor((lat + 90) / 180 * PARALLELS);
-    return meridian * MERIDIANS + parallel;
+
+
+async function divide_in_chunks() {
+    
+    // queries should be done in chunksize = 5x5, map in 30x30
+
+    let chunk_per_dim_query = 5;
+    let chunk_per_dim_map = 30;
+    let chunk_ratio = chunk_per_dim_map / chunk_per_dim_query;
+    let lat_min = -90;
+    let lat_max = 90;
+    let lng_min = -180;
+    let lng_max = 180;
+
+    let markers = {};
+
+    for(let i = 0; i < chunk_per_dim_query; i++) {
+        for(let j = 0; j < chunk_per_dim_query; j++) {
+
+            // calc start and end lat lng
+            let lat_start = lat_min + (lat_max - lat_min) * i / chunk_per_dim_query;
+            let lat_end =   lat_min + (lat_max - lat_min) * (i + 1) / chunk_per_dim_query;
+            let lng_start = lng_min + (lng_max - lng_min) * j / chunk_per_dim_query;
+            let lng_end =   lng_min + (lng_max - lng_min) * (j + 1) / chunk_per_dim_query;
+
+            // create sub chunks arrays
+            for(let k = 0; k < chunk_ratio; k++) {
+                for(let l = 0; l < chunk_ratio; l++) {
+                    // get index of sub chunk incremental
+                    let index = ((i * chunk_per_dim_query) + j) * (chunk_ratio * chunk_ratio) + (k * chunk_ratio) + l;
+                    markers[index] = [];
+                }
+            }
+
+            // get stations
+            let url = `https://api.waqi.info/map/bounds/?latlng=${lat_start},${lng_start},${lat_end},${lng_end}&token=${aqicn_api_key}`;
+            let response = await fetch(url);
+            let data = await response.json();
+            let stations = data.data;
+
+            // TODO FIX
+            function calc_subchunk_index(lat, lng) {
+                let lat_index = Math.floor((lat - lat_start) / (lat_end - lat_start) * chunk_ratio);
+                let lng_index = Math.floor((lng - lng_start) / (lng_end - lng_start) * chunk_ratio);
+                let idx = lat_index * chunk_ratio + lng_index
+                console.log(idx);
+                return idx;
+            }            
+            
+            // create markers
+            for(let pin of stations) {
+                if (pin.aqi == null || pin.aqi == '-') continue;
+                let marker = await create_marker(pin);
+                markers[calc_subchunk_index(marker.lat, marker.lng)].push(marker);
+            }
+
+            //console.log(i,j);
+
+        }
+    }
+
+    return markers;
+    
 }
 
-var markers = {};
+
+async function get_all_stations() {
+    let url = `https://api.waqi.info/v2/map/bounds?latlng=-90,-180,90,180&networks=all&token=${aqicn_api_key}`;
+    let response = await fetch(url);
+    let data = await response.json();
+    return data.data;
+}
+
+
 var map;
 async function init_map() {
 
+    divide_in_chunks();
+
     // init map
     map = new google.maps.Map(document.getElementById('map'),  {  
-        center:  new  google.maps.LatLng(51.505,  -0.09),  
-        mapTypeId:  google.maps.MapTypeId.ROADMAP,  
-        zoom:  11,
+        center: new google.maps.LatLng(51.505, -0.09),  
+        mapTypeId: google.maps.MapTypeId.ROADMAP,  
+        zoom: 11,
         disableDefaultUI: true,
     });  
 
-    // init clusters
-    for (let i = 0; i < MERIDIANS; i++) {
-        for (let j = 0; j < PARALLELS; j++) {
-            let idx = i * MERIDIANS + j;
-            marker_clusters[idx] = new MarkerClusterer(map, []);
-        }        
-    }
 
-    // custom marker on double click
-    map.addListener('dblclick', async (e) => {
-        // get lat, lang, location name
-        let lat = e.latLng.lat();
-        let lng = e.latLng.lng();
-        let url = `https://api.waqi.info/feed/geo:${lat};${lng}/?token=${aqicn_api_key}`;
-        let response = await fetch(url);
-        let data = await response.json();
-        let pin = data.data;
-        marker_click.call({
-            lat: lat,
-            lng: lng,
-            title: "custom marker",
-            aqi: pin.aqi,
-        });
-    });
+    let markers = {};
+    async function update_map() {
 
-    // TODO PERIODICALLY UPDATE MARKERS
-    map.addListener("bounds_changed", async function() {
+
+        let bounds = map.getBounds();
+        let lat_min = bounds.getSouthWest().lat();
+        let lng_min = bounds.getSouthWest().lng();
+        let lat_max = bounds.getNorthEast().lat();
+        let lng_max = bounds.getNorthEast().lng();
+
+        // if map is 30x30 chunks, print chunk id that should be rendered
+        let chunk_per_dim = 30;
+        let chunk_size = (lat_max - lat_min) / chunk_per_dim;
+
+
+
+        /*
 
         // get lat lng bounds
         let bounds = map.getBounds();
@@ -66,18 +121,43 @@ async function init_map() {
         let lat_max = bounds.getNorthEast().lat();
         let lng_min = bounds.getSouthWest().lng();
         let lng_max = bounds.getNorthEast().lng();
-    
-        // TODO PERIODICALLY UPDATE RENDERED MARKERS
-        await get_markers(lat_min, lng_min, lat_max, lng_max).then(data => {            
-    
+
+        // render new markers
+        await get_markers(lat_min, lng_min, lat_max, lng_max).then(async(data) => {            
+
             for(let pin of data) {                
-                if (pin.aqi == null || pin.aqi == '-' || pin.uid in markers)  continue;                
-                markers[pin.uid] = create_marker(pin);              
+
+                if (pin.aqi == null || pin.aqi == '-' || pin.uid in markers) continue;
+                let marker = await create_marker(pin);
+                markers[pin.uid] = marker;
+
             }
-    
+
         });
+
+        // TODO DIVIDE MAP IN CHUNKS
+        // PRELOAD ALL CHUNKS AS INVISIBLE MARKERS
+        // WHEN MAP IS MOVED, GET RENDERED CHUNKS
+        // CHECK HOW MANY STATIONS ARE RENDERED, DO NOT RENDER OTHERS
+
+        // set invisible to all markers   
+        for(let uid in markers) {
+            let marker = markers[uid];
+            let lat = marker.lat;
+            let lng = marker.lng;
+            marker.setVisible(lat >= lat_min && lat <= lat_max && lng >= lng_min && lng <= lng_max);
+        }
+
+        */
+
+    }
+
+    // update map every dragend || 500ms
+    //setInterval(update_map, 500);
+    map.addListener("dragend", update_map);
+    map.addListener("zoom_changed", update_map);
+
     
-    });
 }
 
 var lock = false;
@@ -111,6 +191,7 @@ async function create_marker (pin) {
         aqi:        pin.aqi,            // aqi value
         lat:        pin.lat,            // lat
         lng:        pin.lon,            // lng
+        visible:    false,              // visible
 
         // custom marker label
         label: {
@@ -122,8 +203,7 @@ async function create_marker (pin) {
 
         // custom marker icon
         icon: {
-            url: `./img/${color_idx}-sign.png`,
-            scaledSize: new google.maps.Size(50, 50)
+            url: `./img/${color_idx}-sign.png`
         }                    
 
     });
@@ -148,9 +228,6 @@ async function create_marker (pin) {
             shouldFocus: true,
         });
     });
-
-    let grid_pos = get_marker_grid_position(pin.lat, pin.lon);
-
 
     return marker;
 }
