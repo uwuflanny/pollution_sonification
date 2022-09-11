24dc44fcf47c8673572576c65c07067da6c00a95
uwuFlanny <gianlumiglia@gmail.com>
2022-09-11 17:34:34 +0200

    added modal, started circles pollution

--- misc/map.js
+++ misc/map.js
@@ -18,6 +18,12 @@ async function get_all_stations() {
 
 
 
+async function get_all_stations(){
+    let url = `https://api.waqi.info/v2/map/bounds?latlng=-90,-180,90,180&networks=all&token=${aqicn_api_key}`;
+    let response = await fetch(url);
+    let data = await response.json();
+    return data.data;
+}
 
 
 var map;
@@ -31,11 +37,86 @@ async function init_map() {
         disableDefaultUI: true,
     });  
 
+
+    let chunks_per_dim = 100;
+    async function get_chunk_idx(lat,lng) {
+        let lat_chunk = Math.floor((lat + 90) / 180 * chunks_per_dim);
+        let lng_chunk = Math.floor((lng + 180) / 360 * chunks_per_dim);
+        return lat_chunk * chunks_per_dim + lng_chunk;
+    }
+    await get_all_stations().then(async (data) => {
+        // divide data into chunks
+        // assign each chunk a size based on number of stations and a color based on avg aqi
+
+        // create chunks
+        let chunks = {};
+        for (let i = 0; i < chunks_per_dim * chunks_per_dim; i++) { chunks[i] = []; }
+
+        // load chunks
+        for(let pin of data) {
+            let lat = pin.lat;
+            let lng = pin.lon;
+            let aqi = pin.aqi;
+            let chunk_idx = await get_chunk_idx(lat,lng);
+            chunks[chunk_idx].push(aqi);
+        }
+
+        // for each chunk, calculate avg aqi and size
+        let chunk_sizes = {};
+        let chunk_avg_aqis = {};
+        let chunk_locations = {};
+        for (let i = 0; i < chunks_per_dim * chunks_per_dim; i++) {
+            let chunk = chunks[i];
+            let size = chunk.length;
+            let avg_aqi = chunk.reduce((a, b) => a + b, 0) / size;
+
+            // set location to the nearest point to all the points in the chunk
+            let lat = 0;
+            let lng = 0;
+            for (let p of chunk) {
+                lat += p.lat;
+                lng += p.lng;
+            }
+            lat /= size;
+            lng /= size;
+
+            chunk_locations [i] = {lat: lat, lng: lng};
+            chunk_sizes[i] = size;
+            chunk_avg_aqis[i] = avg_aqi;
+        }
+
+        for(let i = 0; i < chunks_per_dim; i++) {
+            for(let j = 0; j < chunks_per_dim; j++) {
+
+                let idx = i * chunks_per_dim + j;
+
+                let color_idx = Math.floor(chunk_avg_aqis[idx] / 50) > colors.length - 1 ? colors.length - 1 : Math.floor(chunk_avg_aqis[idx] / 50);
+                let color = colors[color_idx];
+
+                // draw circle shape
+                let circle = new google.maps.Circle({
+                    strokeColor: color,
+                    strokeOpacity: 0.8,
+                    strokeWeight: 2,
+                    fillColor: color,
+                    fillOpacity: 0.35,
+                    map: map,
+                    center: chunk_locations[idx],
+                    radius: Math.sqrt(chunk_sizes[idx]) * 100000
+                });
+
+            }
+        }
+
+
+    });
+
+
     // on map click
-    map.addListener('click', () => {
-        // get lat, lng
-        let lat = map.getCenter().lat();
-        let lng = map.getCenter().lng();
+    map.addListener('click', (mouse) => {
+        // get click lat lang
+        let lat = mouse.latLng.lat();
+        let lng = mouse.latLng.lng();
         // get aqi
         let url = `https://api.waqi.info/feed/geo:${lat};${lng}/?token=${aqicn_api_key}`;
         fetch(url)
@@ -43,6 +124,8 @@ async function init_map() {
             .then(data => {
                 let pin = data.data;
                 create_marker(pin);
+                // move map to pin location with animation
+                map.panTo(new google.maps.LatLng(pin.city.geo[0], pin.city.geo[1]));
             });
     });
 
